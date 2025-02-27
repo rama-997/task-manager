85a5636f21f6daf6bd00f96a1b676fe766b357e1
Rama <dubanaev997@gmail.com>
2025-02-27 22:52:55 +0600

    FIX: authDecorator

--- libs/decorators/src/auth.decorator.ts
+++ libs/decorators/src/auth.decorator.ts
@@ -1,10 +1,11 @@
 import { ERoles } from '@src/role/types'
 import { applyDecorators, UseGuards } from '@nestjs/common'
 import { JwtGuard, RoleGuard } from '@src/auth/guards'
+import { RoleDecorator } from '@libs/decorators/role.decorator'
 
 export const AuthDecorator=(...roles:ERoles[])=>{
     if(!roles.length){
         return applyDecorators(UseGuards(JwtGuard))
     }
-    return applyDecorators(UseGuards(JwtGuard,RoleGuard))
+    return applyDecorators(RoleDecorator(...roles),UseGuards(JwtGuard,RoleGuard))
 }
