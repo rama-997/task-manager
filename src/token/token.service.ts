e33f40acd76003259b1ee44dd076b4632acb42f4
Rama <dubanaev997@gmail.com>
2025-03-03 13:40:18 +0600

    ADD: TokenService.emailToken

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { AuthTokens, UserPayload } from '@src/token/types'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class TokenService {
-    constructor(private readonly jwtService: JwtService) {}
+    constructor(
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+    ){}
 
     async authTokens(userPayload: UserPayload): Promise<AuthTokens> {
         const accessToken = await this.jwtService.signAsync(userPayload, {
             expiresIn: '1h',
+            secret:this.configService.getOrThrow<string>('JWT_ACCESS_SECRET')
         })
         const refreshToken = await this.jwtService.signAsync(userPayload, {
             expiresIn: '7d',
+            secret:this.configService.getOrThrow<string>('JWT_REFRESH_SECRET')
         })
 
         return { accessToken, refreshToken }
+    }
+
+    async emailToken(id:string): Promise<string> {
+        return this.jwtService.signAsync({id}, {
+            secret: this.configService.getOrThrow<string>('JWT_EMAIL_SECRET'),
+            expiresIn:'1h'
+        })
     }
 }
