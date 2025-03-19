23e9541d9b973e7b61dc3a35345065c6506bd17d
Rama <dubanaev997@gmail.com>
2025-03-19 23:38:12 +0600

    ADD: AuthController.signUp

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,82 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { TokenService } from '@src/token/token.service'
+import { MailService } from '@src/mail/mail.service'
+import { RoleService } from '@src/role/role.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { User } from '@src/auth/entities'
+import { Repository } from 'typeorm'
+import { Token } from '@src/token/entities'
+import { Role } from '@src/role/entities'
+import { JwtService } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { configServiceMock } from '@libs/common'
+import { MailerService } from '@nestjs-modules/mailer'
+import { mailerMock } from '@src/mail/module-options/mocks'
+import { SignUpDto } from '@src/auth/dto'
+import { signUpDtoMock } from '@src/auth/mocks'
+import { IAuthMess } from '@src/auth/types'
 
 describe('AuthController', () => {
-  let controller: AuthController;
+    let controller: AuthController
+    let authService: AuthService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [AuthController],
+            providers: [
+                AuthService,
+                TokenService,
+                MailService,
+                RoleService,
+                {
+                    provide: getRepositoryToken(User),
+                    useClass: Repository,
+                },
+                {
+                    provide: getRepositoryToken(Token),
+                    useClass: Repository,
+                },
+                {
+                    provide: getRepositoryToken(Role),
+                    useClass: Repository,
+                },
+                JwtService,
+                { provide: ConfigService, useFactory: configServiceMock },
+                { provide: MailerService, useFactory: mailerMock },
+            ],
+        }).compile()
 
-    controller = module.get<AuthController>(AuthController);
-  });
+        controller = module.get<AuthController>(AuthController)
+        authService = module.get<AuthService>(AuthService)
+    })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    it('should be defined authController', () => {
+        expect(controller).toBeDefined()
+    })
+
+    it('should be defined authService', () => {
+        expect(authService).toBeDefined()
+    })
+
+    describe('signUp', () => {
+        let body: SignUpDto
+        let iAuthMess: IAuthMess
+
+        beforeEach(() => {
+            body = signUpDtoMock as SignUpDto
+            iAuthMess = { message: 'send mail to email' }
+        })
+
+        it('should be signed up', async () => {
+            jest.spyOn(authService, 'signUp').mockResolvedValueOnce(iAuthMess)
+
+            const result = await controller.signUp(body)
+
+            expect(authService.signUp).toHaveBeenCalledWith(body)
+            expect(authService.signUp).toHaveBeenCalledTimes(1)
+            expect(result).toEqual({ message: expect.any(String) })
+        })
+    })
+})
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,36 +1,47 @@
 import {
     Body,
-    Controller, Get,
+    Controller,
+    Get,
     HttpCode,
     HttpStatus,
-    Post, Query,
+    Post,
+    Query,
     Res,
 } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { SignInDto, SignUpDto } from '@src/auth/dto'
+import { ResetPassDto, SignInDto, SignUpDto } from '@src/auth/dto'
 import { Response } from 'express'
 import { cookieOptions, resOption } from '@libs/options'
-import { IAccessToken } from '@src/auth/types'
+import { IAccessToken, IAuthMess } from '@src/auth/types'
 import { Cookie, UserAgent } from '@libs/decorators'
 import { ConfigService } from '@nestjs/config'
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService, private readonly configService: ConfigService) {
-    }
+    constructor(
+        private readonly authService: AuthService,
+        private readonly configService: ConfigService,
+    ) {}
 
-    @Post('signup')
+    @Post('sign-up')
     @HttpCode(HttpStatus.OK)
-    async signUp(@Body() signUpDto: SignUpDto): Promise<{ message: string }> {
+    async signUp(@Body() signUpDto: SignUpDto): Promise<IAuthMess> {
         return this.authService.signUp(signUpDto)
     }
 
     @Get('email-confirm')
     @HttpCode(HttpStatus.OK)
-    async emailConfirm(@Query('token') token: string, @UserAgent() agent: string, @Res() res: Response) {
-        const {accessToken, refreshToken} = await this.authService.emailConfirm(token, agent)
+    async emailConfirm(
+        @Query('token') token: string,
+        @UserAgent() agent: string,
+        @Res() res: Response,
+    ) {
+        const { accessToken, refreshToken } =
+            await this.authService.emailConfirm(token, agent)
         res.cookie('token', refreshToken, cookieOptions)
-        res.json({accessToken}).redirect(this.configService.getOrThrow<string>('CLIENT_DOMAIN'))
+        res.json({ accessToken }).redirect(
+            this.configService.getOrThrow<string>('CLIENT_DOMAIN'),
+        )
     }
 
     @Post('signin')
@@ -38,27 +49,36 @@ export class AuthController {
     async signIn(
         @Body() signInDto: SignInDto,
         @Res(resOption) res: Response,
-        @UserAgent() agent: string
+        @UserAgent() agent: string,
     ): Promise<IAccessToken> {
-        const {accessToken, refreshToken} =
-            await this.authService.signIn(signInDto, agent)
+        const { accessToken, refreshToken } = await this.authService.signIn(
+            signInDto,
+            agent,
+        )
         res.cookie('token', refreshToken, cookieOptions)
-        return {accessToken}
+        return { accessToken }
     }
 
     @Get('logout')
-    async logout(@Cookie() cookies:any,@Res() res:Response){
-        const {token}=cookies
+    async logout(@Cookie() cookies: any, @Res() res: Response) {
+        const { token } = cookies
         await this.authService.logout(token)
         res.clearCookie('token')
-        res.status(HttpStatus.OK).json({message:'logout'})
+        res.status(HttpStatus.OK).json({ message: 'logout' })
     }
 
     @Get('refresh-token')
-    async refreshToken(@Cookie() cookies:any,@UserAgent() agent:string,@Res() res:Response){
-        const {token}=cookies
-        const tokens=await this.authService.refreshToken(token,agent)
-        res.cookie('token',tokens.refreshToken,cookieOptions)
-        res.status(HttpStatus.OK).json({message:'Refreshed'})
+    async refreshToken(
+        @Cookie() cookies: any,
+        @UserAgent() agent: string,
+        @Res() res: Response,
+    ) {
+        const { token } = cookies
+        const tokens = await this.authService.refreshToken(token, agent)
+        res.cookie('token', tokens.refreshToken, cookieOptions)
+        res.status(HttpStatus.OK).json({ message: 'Refreshed' })
     }
+
+    @Get('reset-password')
+    async resetPass(@Body() resetPassDto: ResetPassDto) {}
 }
