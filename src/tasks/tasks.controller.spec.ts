d1dd719362a45a027c334f3ae4df3466767cfe21
Rama <dubanaev997@gmail.com>
2025-03-21 23:57:43 +0600

    TEST: TaskController.create

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,20 +1,53 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { TasksController } from './tasks.controller';
-import { TasksService } from './tasks.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { TasksController } from './tasks.controller'
+import { TasksService } from './tasks.service'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Task } from '@src/tasks/entities'
+import { Repository } from 'typeorm'
+import { CreateTaskDto } from '@src/tasks/dto'
+import { createTaskDtoMock, taskMock } from '@src/tasks/mocks'
 
 describe('TasksController', () => {
-  let controller: TasksController;
+    let controller: TasksController
+    let taskService: TasksService
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TasksController],
-      providers: [TasksService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [TasksController],
+            providers: [
+                TasksService,
+                { provide: getRepositoryToken(Task), useClass: Repository },
+            ],
+        }).compile()
 
-    controller = module.get<TasksController>(TasksController);
-  });
+        controller = module.get<TasksController>(TasksController)
+        taskService = module.get<TasksService>(TasksService)
+    })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    it('should be defined providers', () => {
+        expect(controller).toBeDefined()
+        expect(taskService).toBeDefined()
+    })
+
+    describe('create', () => {
+        let createTaskDto: CreateTaskDto
+        let task: Task
+
+        beforeEach(() => {
+            createTaskDto = createTaskDtoMock
+            task = taskMock as Task
+        })
+
+        it('should be created task', async () => {
+            jest.spyOn(taskService, 'create').mockResolvedValueOnce({
+                ...task,
+                ...createTaskDto,
+            })
+
+            const result = await taskService.create(createTaskDto)
+
+            expect(taskService.create).toHaveBeenCalledWith(createTaskDto)
+            expect(result).toEqual({ ...task, ...createTaskDto })
+        })
+    })
+})
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,34 +1,43 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { TasksService } from './tasks.service';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { UpdateTaskDto } from './dto/update-task.dto';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common'
+import { TasksService } from './tasks.service'
+import { CreateTaskDto } from './dto/create-task.dto'
+import { UpdateTaskDto } from './dto/update-task.dto'
+import { Task } from '@src/tasks/entities'
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
+    constructor(private readonly tasksService: TasksService) {}
 
-  @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.tasksService.create(createTaskDto);
-  }
+    @Post()
+    create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+        return this.tasksService.create(createTaskDto)
+    }
 
-  @Get()
-  findAll() {
-    return this.tasksService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.tasksService.findAll()
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.tasksService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.tasksService.findOne(+id)
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.tasksService.update(+id, updateTaskDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
+        return this.tasksService.update(+id, updateTaskDto)
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.tasksService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.tasksService.remove(+id)
+    }
 }
